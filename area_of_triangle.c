c539301a19d94a45c7f4bc0414aaed9ae98148d1
ayushman panda <ayushmanpanda120@gmail.com>
2025-09-07 14:58:12 +0530

    Add files via upload

---
+++ area_of_triangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    float a,s;
+
+    printf("enter base:");
+    scanf("%f",&a);
+    printf("enter height:");
+    scanf("%f",&s);
+
+
+    printf("area of triangle is %f",0.5*a*s);
+
+    return 0 ;
+}
